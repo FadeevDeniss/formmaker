0c1c9769b0e107371fd8f3b9e124fec769c8b656
FadeevDeniss <deniro094@gmail.com>
2022-12-25 19:37:11 +0300

    Change all functions

--- apps/core/static/js/editor.form.js
+++ apps/core/static/js/editor.form.js
@@ -1,108 +1,160 @@
-document.addEventListener("DOMContentLoaded", addHandlers);
+document.addEventListener(
+    "DOMContentLoaded",
+    () => _('#svgPlus').addEventListener(
+        'click', addField)
+    );
 
-function addHandlers() {
-    let add = document.querySelector('#svgPlus');
-
-    add.addEventListener('click', addField);
+function _(s) {
+    return document.querySelector(s);
 }
 
-function addAnswer(e) {
-    e.preventDefault();
-    let cont = e.target.parentElement.parentNode.nextElementSibling;
-    let answer = cont.firstElementChild.cloneNode(true);
-    let t = document.querySelector('div#answerService').firstElementChild;
-    let reg = RegExp(`form-(\\d){1}-`, 'g');
-    let arr = Array.from(cont.querySelectorAll('.answer')).filter(
-        (e) => { return e.className === 'answer' });
-    let num = arr.length;
-    let ch = cont.querySelector('.add');
-    ch.classList.remove('hidden');
-
-    answer.innerHTML = answer.innerHTML.replace(
-        reg, `form-${num}-`);
-    t.value = ++num;
-    answer.classList.remove('hidden');
-    cont.insertBefore(answer, ch)
-
-
-
-
+function a(s) {
+    return document.querySelectorAll(s);
 }
 
-function removeAnswer(e) {
-    e.preventDefault();
-    let a = Array.from(document.querySelectorAll('.answer'));
-    let par = this.parentElement.parentNode;
+function traverse(ch, ind) {
     let reg = RegExp(`form-(\\d){1}-`, 'g');
-    let total = document.querySelector(
-        '#answerService').children['id_form-TOTAL_FORMS'];
-    let root = par.parentNode;
-    let ind = a.indexOf(par);
 
-    if (a.length <= 1) {
-        root.removeChild(par);
-        total.setAttribute('value', `${a.length}`);
-    } else {
-        if (ind < a.length - 1) {
-            root.removeChild(par);
-            Array.from(root.children).slice(ind).map((e) => {
-                e.innerHTML = e.innerHTML.replace(
-                    reg,
-                    `form-${ind - 1}-`
-                );
-                e.children[0].firstElementChild.value = `Answer ${ind}`;
-                ind++;
-            });
-            total.setAttribute('value', `${a.length}`);
-        } else {
-            root.removeChild(par);
-            total.setAttribute('value', `${a.length}`);
-        }
+
+    while(ch !== null) {
+        ch.innerHTML = ch.innerHTML.replaceAll(
+            reg, `form-${ind}-`);
+        ch = ch.nextElementSibling;
     }
 }
 
-function fieldManager() {
-    let res = {};
+function addAnswer(e) {
+    if (e.target.value === 'Choice') {
+        add1(e);
+    }
+    if (e.target.className === 'addAns') {
+        add2(e);
+    }
 
-    res.fields = document.querySelectorAll('.formItem');
-    res.form = document.querySelector('#form-main');
-    res.totalFields = document.querySelector(
-        '#fieldService').children['id_form-TOTAL_FORMS'];
-
-    return res;
 }
 
-function answerManager() {
-    let res = {};
+function add1(e) {
+        let cont = e.target.parentNode.parentNode.nextElementSibling,
+            n = cont.firstElementChild.cloneNode(true),
+            ch = cont.lastElementChild,
+            ind = a('.answer[data-num]').length,
+            input = n.firstElementChild.firstElementChild,
+            add = cont.querySelector('.addAns');
 
-    res.reg = RegExp(`form-(\\d){1}-`, 'g');
-    res.total = document.querySelector(
-        '#answerService').children['id_form-TOTAL_FORMS'];
-    let n1 = p2.nextSibling.cloneNode(true);
+        n.classList.remove('hidden');
+        n.setAttribute('data-num', `${ind}`);
+        input.setAttribute('maxlength', '50');
+        n.lastElementChild.remove();
+        traverse(n.firstElementChild, ind);
+        add.addEventListener('click', addAnswer);
+        _('#answerService').firstElementChild.setAttribute(
+            'value', `${++ind}`);
+        cont.insertBefore(n, ch);
+        ch.classList.remove('hidden');
+}
 
+function add2(e) {
+        let cont = e.target.parentElement.parentElement.parentElement,
+            n = cont.firstElementChild.cloneNode(true),
+            prev = parseInt(cont.parentElement.dataset.num),
+            ch = cont.lastElementChild;
+        
+        n.classList.remove('hidden');
+        n.setAttribute('data-num', `${ind}`);
+        n.lastElementChild.addEventListener('click', removeAnswer);
+        traverse(n.firstElementChild, ind);
+        _('#answerService').firstElementChild.setAttribute(
+            'value', `${++ind}`);
+        cont.insertBefore(n, ch);
+}
 
+function removeField(e) {
+    let cont = e.target.parentNode.parentNode,
+        ind = parseInt(cont.firstElementChild.dataset.num),
+        fields = Array.prototype.slice.call(
+            a('.field[data-num]'), ind + 1);
+
+    for (let i of fields) {
+        ind = --i.dataset.num;
+        traverse(i.firstElementChild, ind);
+    }
+    cont.parentNode.remove();
+    _('#fieldService').firstElementChild.setAttribute(
+        'value', a('.field[data-num]').length
+    )
+}
+
+function copyField(e) {
+    let cont = e.target.parentNode.parentNode,
+        copy = cont.cloneNode(true),
+        next = cont.parentNode.nextElementSibling,
+        div = document.createElement('div'),
+        p = _('#form-main');
+
+    div.classList.add('items');
+    div.appendChild(copy);
+    if (next) {
+        p.insertBefore(div, next);
+        let fields = Array.prototype.slice.call(
+            a(
+                '.field[data-num]'),
+            next.firstElementChild.firstElementChild.dataset.num);
+        for (let i of fields) {
+            traverse(i.firstElementChild, ++i.dataset.num);
+        }
+    } else {
+        ++copy.firstElementChild.dataset.num;
+        _('#fieldService').firstElementChild.setAttribute(
+            'value', a('.field[data-num]').length
+        )
+        p.appendChild(div);
+    }
+}
+
+function removeAnswer(e) {
+    let cont = e.target.parentNode.parentNode,
+        ind = parseInt(cont.dataset.num),
+        answers = Array.prototype.slice.call(a('.answer[data-num]'), ind + 1);
+
+    for ( let i of answers ) {
+        traverse(i, --i.dataset.num);
+    }
+    --_('#answerService').firstElementChild.value;
+    cont.remove();
 }
 
 function addField(e) {
-    e.preventDefault();
 
-    let obj = fieldManager();
-    let fieldNum = obj.fields.length - 1;
-    let newField = obj.fields[fieldNum].cloneNode(true);
-    let reg = RegExp(`form-(\\d){1}-`, 'g');
+    let f = _('.formItem, [data-state="0"]'),
+        n = f.cloneNode(true),
+        p = f.parentNode.parentNode,
+        ch = n.firstElementChild.firstElementChild,
+        btnDel = n.querySelector('#delBtn'),
+        btnCopy = n.querySelector('#copyBtn'),
+        ind = a('.field[data-num]').length,
+        div = document.createElement('div'),
+        options = _('#options'),
+        opNew = options.cloneNode(true);
 
-    newField.classList.remove('hidden');
-    newField.innerHTML = newField.innerHTML.replace(
-        reg, `form-${fieldNum}-`);
-    fieldNum++;
+    div.classList.add('items');
+    btnDel.addEventListener('click', removeField);
+    btnCopy.addEventListener('click', copyField);
+    n.classList.remove('hidden');
+    n.firstElementChild.setAttribute('data-num', `${ind}`);
+    traverse(ch, ind);
+    n.querySelector('.type').addEventListener('input', addAnswer);
+    ++ind;
+    div.appendChild(n);
+    p.appendChild(div);
+    options.remove();
+    opNew.addEventListener('click', addField);
+    _('#fieldService').firstElementChild.setAttribute(
+        'value', `${ind}`);
+    setTimeout(() => {
+        p.lastElementChild.appendChild(opNew);
+        document.querySelector(
+            '#form-main').lastElementChild.lastElementChild.scrollIntoView(
+                {behavior: 'smooth', block: 'start'});
+    }, 500);
 
-    obj.form.append(newField);
-
-    obj.totalFields.setAttribute('value', `${fieldNum + 1}`);
-    newField.children[0].lastElementChild.addEventListener(
-        'input', addAnswer);
-    newField.getElementsByClassName('addAns')[0].addEventListener(
-        'click',
-        addAnswer
-    )
 }
